2a271c33ba9f90cc9a288541919a4c1440faffc4
david-2411 <53522510+david-2411@users.noreply.github.com>
2021-04-14 22:09:59 +1200

    Update LondonTime.ino

--- LondonTime.ino
+++ LondonTime.ino
@@ -1,6 +1,6 @@
 /* ==========================================
  *
- * Use ESP32 WiFi module to get London Time form http request to server: http://worldtimeapi.org/api/timezone/Europe/London.txt
+ * Use ESP32 WiFi library to get London Time form http request to server: http://worldtimeapi.org/api/timezone/Europe/London.txt
  * Then display to 4 digit 7 segment display module
  *
  * Author: David Zheng
