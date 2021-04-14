64efb3eb69d5de3701cbf56b6cbc966fbc4ac476
david-2411 <53522510+david-2411@users.noreply.github.com>
2021-04-14 21:54:07 +1200

    Add files via upload

---
+++ LondonTime.ino
@@ -0,0 +1,93 @@
+#include <Wire.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+#define DigitNum 4 //Total number of digits
+#define TM1650_DISPLAY_BASE 0x34 // Address of the left-most digit 
+#define TM1650_DCTRL_BASE   0x24 // Address of the control register of the left-most digit
+#define delayTime 30 //Time between each http request(seconds)
+
+//Declear subfunction
+void displayToModule (int digit[]);
+
+//Declear global variables
+const char* ssid = "HUAWEI-H106GU";
+const char* password = "P@ssw0rd";
+String serverPath = "http://worldtimeapi.org/api/timezone/Europe/London.txt";
+
+int digitDisplay[10] = {63,6,91,79,102,109,125,7,127,111};//TM1650 data inputs for number 0-9
+
+hw_timer_t * timer = NULL;
+int Time, tempTic;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200); 
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  Wire.begin();
+  for(int i=0; i<DigitNum; i++) {
+      // Turn on display 
+      Wire.beginTransmission(TM1650_DCTRL_BASE+i);      
+      Wire.write((byte) 1);
+      Wire.endTransmission();
+      // Clear display
+      Wire.beginTransmission(TM1650_DISPLAY_BASE+i); 
+      Wire.write((byte) 0);
+      Wire.endTransmission();
+  }
+  Time=-1;
+  tempTic = -delayTime*1000000;
+  timer = timerBegin(0, 80, true); 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int digit[DigitNum];
+  
+  if(timerRead(timer) - tempTic >=delayTime*1000000) {
+    if(WiFi.status()== WL_CONNECTED){
+      HTTPClient http;
+      http.begin(serverPath.c_str());
+      int httpResponseCode = http.GET();
+      while (httpResponseCode!=200) {
+        Serial.print("Error code: ");
+        Serial.println(httpResponseCode);
+        httpResponseCode = http.GET();
+      }
+      Serial.print("HTTP Response code: ");
+      Serial.println(httpResponseCode);
+      String responseData = http.getString();
+      digit[0] = responseData[66]-'0';
+      digit[1] = responseData[67]-'0';
+      digit[2] = responseData[69]-'0';
+      digit[3] = responseData[70]-'0';
+      Serial.println(responseData);
+      Serial.println();
+      http.end();
+    }  
+    tempTic = timerRead(timer);
+  }
+  displayToModule (digit);
+}
+
+void displayToModule (int digit[]) {
+  for(int i=0; i<DigitNum; i++) {
+      Wire.beginTransmission(TM1650_DISPLAY_BASE+i); 
+      
+      if(i==1) { //add dot after second digit
+        Wire.write((byte)digitDisplay[digit[i]] | 0b10000000);
+      } else {
+        Wire.write((byte)digitDisplay[digit[i]]);
+      }      
+      Wire.endTransmission();
+  }
+}
